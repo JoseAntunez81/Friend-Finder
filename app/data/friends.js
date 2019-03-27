51ab2c09ff48b90828625c1cfed3daf405bca6f4
Jose Antunez <jangel5673@yahoo.com>
2019-03-27 15:20:16 -0700

    Friend finder app

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+//Array that holds all friends that have taken the survey
+//Start with a dummy data
+//You could also havean empty array
+
+var friends =[
+    {
+    name:"Brad Johnson",
+    photo:"",
+    scores:["3","5","1","5","3","4","4","5","2","2",]
+    }
+]
+
+module.exports = friends;
